ff95eea87f13432ccc65687b496a19fb585f12f9
Matteo Gamba <matt.gamba.chabert@gmail.com>
2023-06-23 09:17:49 +0000

    S6-R

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-# save this as app.py
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def accueil():
+    return render_template('accueil.html')
+
+@app.route('/equipe1')
+def equipe1():
+    return render_template('equipe1.html')
+
+@app.route('/planning')
+def planning():
+    return render_template('planning.html')
+
+@app.route('/equipe2')
+def equipe2():
+    return redirect(/)
+
+@app.route('/contacts')
+def contacts():
+    return redirect(/)
